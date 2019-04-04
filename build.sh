83d8a26b356abbe1a66cbcf08e577cba422b11d6
Kamyar Mohajerani <kammoh@gmail.com>
2019-04-03 20:48:35 -0400

    my local build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PATH=/usr/local/opt/GNAT/2018/bin:$PATH
+export PATH=$PATH:/usr/local/opt/GNAT/2018/bin
 #~/gnat-gcc-8.1.0-x86_64-apple-darwin15-bin/bin
 ./configure --enable-python --with-llvm-config=/usr/local/opt/llvm/bin/llvm-config && make -j8 && make install || echo "*** ERROR Build Failed ***" ; exit 1
